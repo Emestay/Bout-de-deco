e6937ca721b3b413b7ed5b7f396611da61eeaedf
Emestay <103575914+Emestay@users.noreply.github.com>
2022-12-24 16:03:12 +0100

    CommentsAboutEach

--- navbar.vue
+++ navbar.vue
@@ -1,39 +1,43 @@
 Copy code
 <template>
-  <nav>
-    <!-- navbar items -->
-    <ul>
-      <li>
-        <a href="#">Home</a>
-      </li>
-      <li>
-        <a href="#">About</a>
-      </li>
-      <!-- dropdown menu -->
-      <li>
-        <a href="#" @click="showDropdown = !showDropdown">
-          Services
-          <i class="fas fa-caret-down"></i>
-        </a>
-        <ul v-if="showDropdown">
-          <li><a href="#">Design</a></li>
-          <li><a href="#">Development</a></li>
-          <li><a href="#">Consulting</a></li>
+    <nav>
+        <!-- navbar items -->
+        <ul>
+            <li>
+                <a href="#">Home</a>
+            </li>
+            <li>
+                <a href="#">About</a>
+            </li>
+            <!-- dropdown menu -->
+            <li>
+                <a href="#" @click="showDropdown = !showDropdown">
+                    Services
+                    <i class="fas fa-caret-down"></i>
+                </a>
+                <ul v-if="showDropdown">
+                    <li><a href="#">Design</a></li>
+                    <li><a href="#">Development</a></li>
+                    <li><a href="#">Consulting</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">Contact</a>
+            </li>
         </ul>
-      </li>
-      <li>
-        <a href="#">Contact</a>
-      </li>
-    </ul>
-  </nav>
+    </nav>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      showDropdown: false
+    data() {
+        return {
+            showDropdown: false
+        }
     }
-  }
 }
 </script>
+
+<!-- This navbar has a dropdown menu that appears when the "Services" link is clicked, and disappears when the link is clicked again. The dropdown menu is displayed using a conditional (v-if) directive, which only renders the menu if the showDropdown data property is true. The showDropdown property is toggled using a click event handler (@click) that flips its value between true and false using the not operator (!).
+
+You can customize the navbar by adding more links or changing the styles using CSS. You can also use the v-for directive to dynamically generate the links from an array of data. -->
--- navbarDrop.vue
+++ navbarDrop.vue
@@ -1,66 +1,72 @@
 <template>
     <nav>
-      <ul>
-        <!-- generate links using v-for -->
-        <li v-for="(item, index) in navItems" :key="index">
-          <!-- dropdown menu -->
-          <template v-if="item.dropdown">
-            <a href="#" @click="showDropdown = !showDropdown">
-              {{ item.label }}
-              <i class="fas fa-caret-down"></i>
-            </a>
-            <ul v-if="showDropdown">
-              <li v-for="subitem in item.dropdown" :key="subitem.label">
-                <a :href="subitem.href">{{ subitem.label }}</a>
-              </li>
-            </ul>
-          </template>
-          <!-- regular link -->
-          <template v-else>
-            <a :href="item.href">{{ item.label }}</a>
-          </template>
-        </li>
-      </ul>
+        <ul>
+            <!-- generate links using v-for -->
+            <li v-for="(item, index) in navItems" :key="index">
+                <!-- dropdown menu -->
+                <template v-if="item.dropdown">
+                    <a href="#" @click="showDropdown = !showDropdown">
+                        {{ item.label }}
+                        <i class="fas fa-caret-down"></i>
+                    </a>
+                    <ul v-if="showDropdown">
+                        <li v-for="subitem in item.dropdown" :key="subitem.label">
+                            <a :href="subitem.href">{{ subitem.label }}</a>
+                        </li>
+                    </ul>
+                </template>
+                <!-- regular link -->
+                <template v-else>
+                    <a :href="item.href">{{ item.label }}</a>
+                </template>
+            </li>
+        </ul>
     </nav>
-  </template>
+</template>
   
-  <script>
-  export default {
+<script>
+export default {
     data() {
-      return {
-        showDropdown: false,
-        navItems: [
-          {
-            label: 'Home',
-            href: '#'
-          },
-          {
-            label: 'About',
-            href: '#'
-          },
-          {
-            label: 'Services',
-            dropdown: [
-              {
-                label: 'Design',
-                href: '#'
-              },
-              {
-                label: 'Development',
-                href: '#'
-              },
-              {
-                label: 'Consulting',
-                href: '#'
-              }
+        return {
+            showDropdown: false,
+            navItems: [
+                {
+                    label: 'Home',
+                    href: '#'
+                },
+                {
+                    label: 'About',
+                    href: '#'
+                },
+                {
+                    label: 'Services',
+                    dropdown: [
+                        {
+                            label: 'Design',
+                            href: '#'
+                        },
+                        {
+                            label: 'Development',
+                            href: '#'
+                        },
+                        {
+                            label: 'Consulting',
+                            href: '#'
+                        }
+                    ]
+                },
+                {
+                    label: 'Contact',
+                    href: '#'
+                }
             ]
-          },
-          {
-            label: 'Contact',
-            href: '#'
-          }
-        ]
-      }
+        }
     }
-  }
-  </script>
+}
+</script>
+
+<!-- In this example, the navItems data property is an array of objects that represent the links in the navbar. Each object has a label property for the link text and a href property for the link destination. If the object has a dropdown property, it is treated as a dropdown menu with subitems.
+
+The v-for directive is used to loop through the navItems array and generate a li element for each item. The v-if directive is used to conditionally render the dropdown menu or the regular link based on whether the item has a dropdown property. The v-for directive is also used to loop through the subitems in the dropdown menu and generate a li element for each subitem.
+
+You can customize the navbar by adding more links or changing the styles using CSS. You can also add more properties to the navItems objects to store additional information about the links (e.g. an icon, a target attribute, etc.). -->
